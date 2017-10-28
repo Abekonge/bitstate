83d9f1960c214a751bb2f843c3021503c16ad498
Abekonge <davidbirk@gmail.com>
2017-10-28 09:20:45 +0200

    refactor wordcount script

--- Draft/wcount.sh
+++ Draft/wcount.sh
@@ -1,1 +1,7 @@
-n=0;for file in ./*/*.txt; do echo $file; x=$(cat "$file" | wc -w);n=$((n+x)); done; echo $n
+n=0;
+for file in ./*/*.txt;
+do echo $file;
+x=$(cat "$file" | wc -w);
+n=$((n+x));
+done;
+echo $n
